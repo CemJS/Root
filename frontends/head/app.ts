df9216c9f0ddb1a42c9aa50b8e38815a8670e879
Betarost <betarost@yandex.ru>
2023-06-15 16:01:13 +0300

    Add MicroFront

---
+++ frontends/head/app.ts
@@ -0,0 +1,4 @@
+import { load } from "cemjs-core";
+import { micro } from "./src";
+
+load(micro)
---
+++ frontends/head/src/display.tsx
@@ -0,0 +1,9 @@
+import { Cemjsx } from "cemjs-core"
+
+export const display = function () {
+    return (
+        <div class="welcome">
+            <h2 class="welcome_title">Header</h2>
+        </div>
+    )
+}
