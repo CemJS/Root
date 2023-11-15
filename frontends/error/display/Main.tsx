7860b9c8e60ff48b32c78cb6f7d1ce680f937137
Betarost <betarost@yandex.ru>
2023-11-15 11:29:52 +0300

    .

--- frontends/error/display/Main.tsx
+++ frontends/error/display/Main.tsx
@@ -1,12 +1,6 @@
 import { Cemjsx, Fn, Static } from "cemjs-all"
 
 export default function () {
-
-
-    Static.setInterval = setInterval(() => {
-        console.log('=554709=', "setTimeout")
-    }, 2000);
-
     return (
         <div class="error_wrap">
             <p class="error_text">Страница не найдена</p>
--- frontends/home/navigation.tsx
+++ frontends/home/navigation.tsx
@@ -2,6 +2,5 @@ import { Cemjsx, Variable, Fn } from "cemjs-all"
 import Main from "./display/Main"
 
 export default function () {
-  console.log('=e9b18e=', Variable)
   return <Main />
 }
