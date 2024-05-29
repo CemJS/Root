7936423c3dcf98ec96a2cf1e13d574aa2faa790a
Pavel <pavel.glugxix@gmail.com>
2024-05-29 10:14:36 +0300

    Update error page

--- frontends/error/display/Main.tsx
+++ frontends/error/display/Main.tsx
@@ -6,17 +6,16 @@ export default function () {
     }
 
     return (
-        <div class="container mx-auto flex-auto flex items-center">
-            <div class="flex flex-col md:w-1/2 m-auto content-center gap-6 bg-red-400 rounded-xl p-4 md:p-8">
-                <h1 class="text-4xl md:text-7xl font-bold text-center">Ошибка 404</h1>
-                <p class="text-2xl md:text-4xl font-medium text-center">Страница не найдена</p>
-                <div class="m-auto">
-                    <button
-                        onclick={click}
-                        class="text-2xl md:text-4xl font-bold bg-red-600 p-4 rounded-xl active:bg-red-700 ease-in-out duration-150"
-                    >Вернуться на главную</button>
-                </div>
-            </div>
+        <div class="container mx-auto flex-auto flex flex-col justify-center items-center gap-5">
+            <h1 class="text-7xl font-semibold">Error 404</h1>
+            <p class="text-6xl font-bold">Страница не найдена</p>
+            <p class="text-4xl">Вернитесь на главную страницу</p>
+            <button
+                onclick={click}
+                class="text-4xl font-medium text-black bg-white p-4 rounded-2xl transition-all hover:bg-slate-100"
+            >
+                <span>На главную</span>
+            </button>
         </div>
     )
 }
